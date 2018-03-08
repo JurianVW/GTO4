54f74f7b2dd20a51dc473a82b89b01c6381ac3b8
Unknown <jurianvanwoerkom@hotmail.com>
2018-03-08 13:22:32 +0100

    Finished Workshop: Resources

---
+++ GTO4/Assets/Scripts/Resource.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+
+    public float gold;
+    public float silver;
+    public float bronze;
+
+    public delegate void FloatChange();
+    public static event FloatChange ResourceValueChanged;
+    void Start()
+    {
+        SetGold(25);
+        SetSilver(45);
+        SetBronze(10);
+    }
+
+    public void SetGold(int amount)
+    {
+        gold += amount;
+        ResourceChanged();
+    }
+
+    public void SetSilver(int amount)
+    {
+        silver += amount;
+        ResourceChanged();
+    }
+
+    public void SetBronze(int amount)
+    {
+        bronze += amount;
+        ResourceChanged();
+    }
+
+    private void ResourceChanged()
+    {
+        if (ResourceValueChanged != null)
+        {
+            ResourceValueChanged();
+        }
+    }
+}
---
+++ GTO4/Assets/Scripts/ResourceUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceUI : MonoBehaviour
+{
+
+    public Resource resource;
+    [Space(10)]
+    public Text goldValue;
+    public Text silverValue;
+    public Text bronzeValue;
+
+    void Start()
+    {
+        Resource.ResourceValueChanged += UpdateUI;
+    }
+
+    private void UpdateUI()
+    {
+        goldValue.text = resource.gold.ToString();
+        silverValue.text = resource.silver.ToString();
+        bronzeValue.text = resource.bronze.ToString();
+    }
+}
